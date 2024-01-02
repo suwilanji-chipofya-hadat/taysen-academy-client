2bfc3cd05d28682d327ab05e71e3096b7a31ccb4
Suwilanji Chipofya <138398395+suwilanji-chipofya-hadat@users.noreply.github.com>
2024-01-02 16:20:21 +0200

    Update root.jsx

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -8,7 +8,7 @@ export default function Root() {
         <div className="w-full h-screen flex flex-col text-gray-300 bg-black sm:flex-row-reverse">
             <div className="flex flex-col w-full flex-grow">
                 <Top/>
-                <div className="w-full flex-grow overflow-y-scroll bg-black px-3">
+                <div className="w-full flex-grow overflow-y-scroll bg-black px-3 pb-[300px]">
                     <Outlet/>
                 </div>
             </div>
