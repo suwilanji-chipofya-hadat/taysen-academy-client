07c96b98b1b7d996dcc1f3c4b8d90c856b18b901
Suwilanji Chipofya <138398395+suwilanji-chipofya-hadat@users.noreply.github.com>
2024-01-02 16:15:52 +0200

    Update root.jsx

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -6,7 +6,7 @@ import { Analytics } from '@vercel/analytics/react';
 export default function Root() {
     return (
         <div className="w-full h-screen flex flex-col text-gray-300 bg-black sm:flex-row-reverse">
-            <div className="flex flex-col w-full flex-grow mb-[150px]">
+            <div className="flex flex-col w-full flex-grow mb-[350px]">
                 <Top/>
                 <div className="w-full flex-grow overflow-y-scroll bg-black px-3">
                     <Outlet/>
