ab8bb403df07c851f71600d3681ff439cb62c4f9
Suwilanji Chipofya <138398395+suwilanji-chipofya-hadat@users.noreply.github.com>
2024-01-02 16:36:20 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export default function Navbar() {
         console.log(location.pathname)
     }, [location])
     return (
-        <div className="sticky bottom-0 sm:static w-full flex justify-around p-3 sm:w-fit sm:flex-col sm:justify-normal sm:space-y-7 sm:pt-11 sm:items-center">
+        <div className="sticky bottom-0 sm:static w-full flex justify-around p-3 sm:w-fit sm:flex-col sm:justify-normal sm:space-y-7 sm:pt-11 sm:items-center sm:h-full">
             <Link to={'/'} className={`${location.pathname === "/" && "text-white"} flex flex-col items-center space-y-2`}>
                 <BsStar className="text-2xl font-bold"/>
                 <p className="text-sm font-semibold">Featured</p>
@@ -27,7 +27,7 @@ export default function Navbar() {
                 <BsHeart className="text-2xl font-bold"/>
                 <p className="text-sm font-semibold whitespace-nowrap">Wishlist</p>
             </Link>
-            <Link to={'/account'} className={`${location.pathname === "/account" && "text-white"} flex flex-col items-center space-y-2 flex-item sm:self-end`}>
+            <Link to={'/account'} className={`${location.pathname === "/account" && "text-white"} flex flex-col items-center space-y-2 flex-item sm:justify-self-end`}>
                 <BsPersonCircle className="text-2xl font-bold"/>
                 <p className="text-sm font-semibold">Account</p>
             </Link>
