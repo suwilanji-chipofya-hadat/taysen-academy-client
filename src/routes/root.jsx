c874a31661c3e32a47d33b47dde5b99277e81bb2
Suwilanji Chipofya <138398395+suwilanji-chipofya-hadat@users.noreply.github.com>
2024-01-02 16:37:23 +0200

    Update root.jsx

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -12,7 +12,7 @@ export default function Root() {
                     <Outlet/>
                 </div>
             </div>
-            <div className="h-fit w-full sticky bottom-0 sm:static sm:w-fit bg-gray-900">
+            <div className="h-fit sm:h-full w-full sticky bottom-0 sm:static sm:w-fit bg-gray-900">
                 <Navbar/>
             </div>
             <Analytics />
